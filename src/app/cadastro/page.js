9f1f191bc2242776e0474dcaa676140f285051ad
Kenned Ferreira <74821126+kennedfer@users.noreply.github.com>
2024-12-11 17:40:00 +0000

    feat: Iniciando a página de cadastro

---
+++ src/app/cadastro/page.js
@@ -0,0 +1,19 @@
+"use client"
+import DeviceItem from "@/components/devices/DeviceItem";
+import { Header } from "@/components/header/Header";
+import { useEffect, useState } from "react";
+// const 
+
+export default function Home() {
+
+  return (
+    <body>
+      <main>
+        <div className="d-flex flex-column space-y-4 p-4">
+        Kennedfer
+        </div>
+      </main>
+    </body>
+
+  );
+}
